b0b69aa1e5e69bee8bde595dab26a68f949f7c6c
Zachary Boyd <zachary@zacharyboyd.nyc>
2022-03-24 16:07:32 -0400

    adds functions for extracting metaplex metadata

--- bin/get-tokens-created-by.js
+++ bin/get-tokens-created-by.js
@@ -11,22 +11,26 @@ const argv = require('minimist')(process.argv.slice(2));
   const results = [];
   //                                                    v--- Controls how many concurrent calls to the chain we make to get info.
   for await (const result of getAccountBatch(tokenIds, 25)) {
-    result.meta = [];
-    const metas = new Set();
-    for await (const meta of getTokenMetadata(result.id)) {
-      metas.add(JSON.stringify(meta));
+    try {
+      result.meta = [];
+      const metas = new Set();
+      for await (const meta of getTokenMetadata(result.id)) {
+        metas.add(JSON.stringify(meta));
+      }
+
+      // This is to remove duplicate meta objects
+      result.meta = Array.from(metas.values()).map((s) => JSON.parse(s));
+
+      process.stdout.write(
+        JSON.stringify(result) + "\n"
+      );
+    } catch (err) {
+      console.error(err.stack);
     }
-
-    // This is to remove duplicate meta objects
-    result.meta = Array.from(metas.values()).map((s) => JSON.parse(s));
-
-    process.stdout.write(
-      JSON.stringify(result) + "\n"
-    )
   }
 
   process.exit(0);
 })().catch(err => {
-  process.stderr.end(err.stack);
+  console.error(err.stack);
   process.exit(1);
 });
